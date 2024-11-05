bea714abb545d1d2da9661b5346be9745b3adb61
Indhuja-git <sindhujapvt@gmail.com>
2024-11-05 17:05:27 +0530

    code: hangman

--- hangman.py
+++ hangman.py
@@ -4,10 +4,30 @@ word_list = ["apple", "camel", "blue"]
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
-guess = input("Guess a letter: ").lower()
 
-for letter in guess:
-    if letter in chosen_word:
-        print("Right")
-    else:
-        print("Wrong")
+placeholder = ""
+word_length = len(chosen_word)
+for word in range(word_length):
+    placeholder += "-"
+print(placeholder)
+
+is_game_over = False
+correct_letters = []
+
+while not is_game_over:
+    guess = input("Guess a letter: ").lower()
+
+    display = ""
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+    print(display)
+
+    if "_" not in display:
+        is_game_over = True
+        print("You Won!")
